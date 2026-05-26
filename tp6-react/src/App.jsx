d9c2cf29e3f32b08e0d7265fa163565e4575009b
Marco Farfan Arias <mark20032202@gmail.com>
2026-05-25 21:28:02 -0300

    build: limpiar App.jsx y renderizar instancias de prueba de las tarjetas

--- tp6-react/src/App.jsx
+++ tp6-react/src/App.jsx
@@ -17,10 +17,10 @@ function App() {
         <p>Productos en oferta por tiempo limitado.</p>
         
         <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-          <Card titulo="Mouse Gamer" descripcion="Mouse RGB con sensor óptico." imagen="https://picsum.photos/id/10/300/200" precio={4500} />
-          <Card titulo="Teclado Mecánico" descripcion="Teclado compacto con switches red." imagen="https://picsum.photos/id/48/300/200" precio={12000} />
-          <Card titulo="Mousepad XXL" descripcion="Superficie de tela suave." imagen="https://picsum.photos/id/60/300/200" />
-          <Card titulo="Auriculares Pro" descripcion="Sonido envolvente." imagen="https://picsum.photos/id/26/300/200" precio={8500}>
+          <Card titulo="Mouse Gamer" descripcion="Mouse RGB con sensor óptico." imagen="https://imgs.search.brave.com/qmG8IHBlkoNjAwZsRKDF8CLTtnkJKTaUGkBBXQ0CJ14/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZWRy/YWdvbnNob3AuY29t/L2Nkbi9zaG9wL2Zp/bGVzL1BSRURBVE9S/TTYxMlBST2dhbWlu/Z21vdXNlXzEucG5n/P3Y9MTc2MjQ2MzY1/NSZ3aWR0aD01MzM" precio={4500} />
+          <Card titulo="Teclado Mecánico" descripcion="Teclado compacto con switches red." imagen="https://imgs.search.brave.com/2X2AzYeZKQ8T3K7N6nlk5wq4NPMgDJkVOXL6H4dFIaQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5waWNoYXUuY29t/LmJyL21lZGlhL2Nh/dGFsb2cvcHJvZHVj/dC9jYWNoZS9lZjcy/ZDNjMjc4NjQ1MTBl/NWQ0YzBjZTY5YmFk/ZTI1OS90L3AvdHBt/a2J2cmQ1NjI1MjIw/M18xLmpwZw" precio={12000} />
+          <Card titulo="Mousepad XXL" descripcion="Superficie de tela suave." imagen="https://imgs.search.brave.com/NnqXLJGVx9VSorPCwy7ooCZnbx2xiKEDHvPfp92BsSw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aGVt/b3VzZXBhZGNvbXBh/bnkuY29tL2Nkbi9z/aG9wL2ZpbGVzL21h/cHptLWJ3LWFlcm8t/bHJnLWFlcm8tdGh1/bWJrLmpwZz92PTE3/NjQ4Njc2Mjgmd2lk/dGg9NDQ3Mg" />
+          <Card titulo="Auriculares Pro" descripcion="Sonido envolvente." imagen="https://imgs.search.brave.com/DqqoD1BPcCyTm17mfGxomID8HHxYcjXU2Bb3xaAXFy8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9odHRw/Mi5tbHN0YXRpYy5j/b20vRF9RX05QXzJY/Xzk4NzExMC1NTEE5/NjEwNDAyMTMyMV8x/MDIwMjUtVi53ZWJw" precio={8500}>
             <span style={{ backgroundColor: '#ff4655', padding: '2px 5px', borderRadius: '4px', fontSize: '12px' }}>🔥 ¡Más Vendido!</span>
           </Card>
         </div>
